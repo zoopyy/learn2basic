e541efa88a81e3e604b9108856b092c9bb7d546a
Andrei R <zoopyy@gmail.com>
2016-07-05 22:37:42 +0300

    -Initial code.

---
+++ app/src/main/java/com/learn2basic/LinuxActivity.java
@@ -0,0 +1,13 @@
+package com.learn2basic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinuxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linux);
+    }
+}
